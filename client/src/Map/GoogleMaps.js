ccc8e2408c28e40070e5d7df55ff538b9f4f63c4
Christian Lopez <christian.lopez023@mymdc.net>
2018-07-09 16:46:54 -0400

    deploy attempt

--- client/src/Map/GoogleMaps.js
+++ client/src/Map/GoogleMaps.js
@@ -3,7 +3,7 @@ import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 import Navbar from "../NavAndFoot/Navbar";
 import { connect } from "react-redux";
 import { getVacations } from "../redux/vacations";
-import Geocode from "react-geocode";
+// import Geocode from "react-geocode";
 import Geocoder from "geocoder";
 import Slide from 'react-reveal/Slide';
 
@@ -21,44 +21,44 @@ class GoogleMaps extends Component {
   }
 
   componentDidMount = () => {
-    this.props.getVacations().then(() => {
-        const coordinates = this.props.vacations.vacations.map(coordinate => {
-            const convert = Geocode.fromAddress(coordinate.location);
-            return convert;
-          })
-          Promise.all(coordinates).then(locations => {
-            const markers = locations.map(place => {
-              const lat = place.results[0].geometry.location.lat;
-              const lng = place.results[0].geometry.location.lng;
-              return (
-                  <Marker key={place.results[0].place_id} 
-                  position={{lat: lat, lng: lng}} 
-                  animation={2} 
-                  onClick={() => 
-                    this.state.zoomedOut ? 
-                    this.setState( prevState => {
-                      return {
-                        zoom: 14, 
-                        lat: lat, 
-                        lng: lng, 
-                        zoomedOut: !prevState.zoomedOut
-                        }
-                    })
-                     : this.setState(prevState => {
-                      return {
-                        zoomedOut: !prevState.zoomedOut,
-                        lat: 39.50,
-                        lng: -94.35,
-                        zoom: 4
-                      }
-                    })}/>
-              )
-            })
-            this.setState({
-              coords: markers
-            })
-          })
-     });
+    // this.props.getVacations().then(() => {
+    //     const coordinates = this.props.vacations.vacations.map(coordinate => {
+    //         const convert = Geocode.fromAddress(coordinate.location);
+    //         return convert;
+    //       })
+    //       Promise.all(coordinates).then(locations => {
+    //         const markers = locations.map(place => {
+    //           const lat = place.results[0].geometry.location.lat;
+    //           const lng = place.results[0].geometry.location.lng;
+    //           return (
+    //               <Marker key={place.results[0].place_id} 
+    //               position={{lat: lat, lng: lng}} 
+    //               animation={2} 
+    //               onClick={() => 
+    //                 this.state.zoomedOut ? 
+    //                 this.setState( prevState => {
+    //                   return {
+    //                     zoom: 14, 
+    //                     lat: lat, 
+    //                     lng: lng, 
+    //                     zoomedOut: !prevState.zoomedOut
+    //                     }
+    //                 })
+    //                  : this.setState(prevState => {
+    //                   return {
+    //                     zoomedOut: !prevState.zoomedOut,
+    //                     lat: 39.50,
+    //                     lng: -94.35,
+    //                     zoom: 4
+    //                   }
+    //                 })}/>
+    //           )
+    //         })
+    //         this.setState({
+    //           coords: markers
+    //         })
+    //       })
+    //  });
   }
 
   render() {
