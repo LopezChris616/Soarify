dbbd5e84e41e9c4c830e69fe8be70227340a3a1d
Christian Lopez <christian.lopez023@mymdc.net>
2018-10-09 22:00:40 -0400

    finished navbar implementation

--- client/src/NavAndFoot/Navbar.js
+++ client/src/NavAndFoot/Navbar.js
@@ -1,28 +1,48 @@
 import React, {Component} from "react";
-import NavbarItems from "./NavbarItems";
+import { connect } from "react-redux";
+import { Link } from  "react-router-dom";
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-class Navbar extends Component {
+class Navigation extends Component {
     constructor(props){
         super(props);
         this.state = {
-            mobileOpen: false
+            navCollapsed: false
         }
     }
 
-    handleDrawerToggle = () => {
-      this.setState(state => ({ mobileOpen: !state.mobileOpen }));
-    };
+    navbarToggle = () => {
+      this.setState({
+        navCollapsed: !this.state.navCollapsed
+      })
+    }
 
     render(){
+      const {isAuthenticated} = this.props.user;
         return(
           <div>
-
+            <Navbar color="dark" dark>
+              <NavbarBrand tag={Link} to="/" className="mr-auto">Soarify</NavbarBrand>
+              <NavbarToggler onClick={this.navbarToggle} className="mr-2" />
+              <Collapse isOpen={this.state.navCollapsed} navbar>
+                <Nav navbar>
+                  <NavItem>
+                    {!isAuthenticated && <NavLink tag={Link} to="/signup">Sign Up</NavLink>}
+                    {isAuthenticated && <NavLink tag={Link} to="/createVacation">View Your Vacation Plans</NavLink>}
+                  </NavItem>
+                  <NavItem>
+                    {!isAuthenticated && <NavLink tag={Link} to="/login">Login</NavLink>}
+                    {isAuthenticated && <NavLink tag={Link} to="/googleMaps">View On Google Maps</NavLink>}
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </Navbar>
           </div>
         )
     }
 }
 
-export default Navbar;
+export default connect(state => state, {})(Navigation);
 
 
       {/* <AppBar className={classes.appBar}>
